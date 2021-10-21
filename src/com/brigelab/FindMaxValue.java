7096d101ba17bc75ce7ee11cf508a90b71c8586e
Ashish642 <88175180+Ashish642@users.noreply.github.com>
2021-10-21 19:30:02 +0530

    Update FindMaxValue.java

--- src/com/brigelab/FindMaxValue.java
+++ src/com/brigelab/FindMaxValue.java
@@ -9,7 +9,7 @@ public class FindMaxValue {
     public static Integer maxOfInteger(Integer a1,Integer a2, Integer a3) {
         Integer max = a1;
         if (a2.compareTo(max) > 0)
-            max = 2;
+            max = a2;
         if (a3.compareTo(max) > 0)
             max = a3;
         return max;
